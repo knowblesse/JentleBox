fc1abdc5af74fe134f7931f89ea063bbd11b758e
Knowblesse <knowblesse@gmail.com>
2022-10-12 19:03:01 +0900

    SY function example added

---
+++ SY_function_example.ino
@@ -0,0 +1,10 @@
+void UIFunction(ExpParam a)
+{
+	a.cs_duration = 10;
+	a.habituation_time = 120;
+	a.us_duration = 2.5;
+	a.us_onset = 7.5;
+	a.iti_duration_max = 60;
+	a.iti_duration_min = 60;
+	a.num_trial = 20;
+}
